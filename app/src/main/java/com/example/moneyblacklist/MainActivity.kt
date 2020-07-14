2cc61f5335e88f8a9b789194b3fa27b57f55c43d
pablopp1997 <pablo.paradinas.prieto@gmail.com>
2020-07-14 17:43:16 +0200

    Cambios quitado settings y cambiado icono para añadir

--- app/src/main/java/com/example/moneyblacklist/MainActivity.kt
+++ app/src/main/java/com/example/moneyblacklist/MainActivity.kt
@@ -28,23 +28,18 @@ class MainActivity : AppCompatActivity() {
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
+            //AQUI NOS LLEVARIA A LA PANTALLA DE AÑADIR UN NUEVO DEUDOR (PERSONA)
+
         }
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
